8f55ca7f90ce1d0d3b94b97e95797da8749f92b3
Antonio Rodrigues Benítez <antonio.rodriguez@udg.edu>
2026-03-13 11:03:23 +0100

    Funcions necessaries

--- js/canvasgame.js
+++ js/canvasgame.js
@@ -4,7 +4,19 @@ import {clickCard, gameItems, selectCards, startGame, initCard, saveGame} from "
 let game = $('#game');
 let canvas = game[0].getContext('2d');
 if (canvas){
-    // Inicialitzar CANVAS
     game.attr("width", 800);
     game.attr("height", 600);
+    start();
+    update();
+}
+
+function start(){
+    // TODO: Carregar recursos
+    // TODO: Vincular events
+}
+
+function update(){
+    checkInput();
+    draw();
+    // TODO: Com es pot fer per que això sigui LOOP
 }
